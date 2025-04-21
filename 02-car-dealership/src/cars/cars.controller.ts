93751044417a6082dc86247906b0eff4e5906c13
nicolasvior <ngvior@gmail.com>
2025-04-21 09:52:22 -0300

    controllers-modules-&-service

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,38 +1,21 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
-import { NotFoundError } from 'rxjs';
+import { Controller, Get, NotFoundException, Param, ParseIntPipe } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
-  private cars = [
-    {
-      id: 0,
-      brand: 'Toyota',
-      model: 'Corolla',
-    },
-    {
-      id: 1,
-      brand: 'Honda',
-      model: 'Civic',
-    },
-    {
-      id: 2,
-      brand: 'Ford',
-      model: 'Mustang',
-    },
-  ];
+
+    constructor(
+        private readonly carsService: CarsService,
+    ) {}
 
   @Get()
   getAllCars() {
-    return this.cars;
+    return this.carsService.findAll();
   }
 
   @Get(':id')
-  getCarById(@Param('id') id) {
-    // return { id };
-    const car = this.cars.find((car) => car.id === +id);
-    if (!car) {
-      throw new NotFoundException(`Car with id ${id} not found`);
-    }
-    return car;
+  getCarById(@Param('id', ParseIntPipe) id: number) {
+    console.log({id});
+    return this.carsService.findOneById(id);
   }
 }
