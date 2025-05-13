74b9b9b1b9f1b8b9da18f2124eed6bbeb08a6e91
nicolasvior <ngvior@gmail.com>
2025-05-13 15:20:47 -0300

    connect nest w mongo

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -2,6 +2,8 @@ import { join } from 'path'; //paquetes de  node van primero
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
+import mongoose from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -9,6 +11,9 @@ import { PokemonModule } from './pokemon/pokemon.module';
       rootPath: join(__dirname, '..', 'public'),
       // rootPath: `${__dirname}/../public`,
     }),
+
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemons'),
+
     PokemonModule,
   ],
 })
