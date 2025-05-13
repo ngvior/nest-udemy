b8c3bf3a1403089ae6904da7ff657d9fa7b7dc57
nicolasvior <ngvior@gmail.com>
2025-05-13 15:42:43 -0300

    created pokemon schema

--- 03-pokedex/src/pokemon/entities/pokemon.entity.ts
+++ 03-pokedex/src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,21 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Pokemon extends Document {
+  // private id: string; // Mongo genera un id automaticamente
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  private no: number;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  private name: string;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
