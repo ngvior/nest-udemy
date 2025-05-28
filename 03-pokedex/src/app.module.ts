1d465782634e9f4daeb5ff3f3d45af66ac66d2d1
ngvior <ngvior@gmail.com>
2025-05-28 14:39:08 -0300

    #103 setting up enviroment variables to the project

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -1,20 +1,23 @@
 import { join } from 'path'; //paquetes de  node van primero
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { PokemonModule } from './pokemon/pokemon.module';
-import mongoose from 'mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
+import { PokemonModule } from './pokemon/pokemon.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
       // rootPath: `${__dirname}/../public`,
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    // MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB!),
 
     PokemonModule,
 
@@ -22,5 +25,7 @@ import { SeedModule } from './seed/seed.module';
 
     SeedModule,
   ],
-})  
-export class AppModule {}
+})
+export class AppModule {
+  constructor(private readonly configService: ConfigService) {}
+}
